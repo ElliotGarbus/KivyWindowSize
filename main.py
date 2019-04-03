8c0999a97ef7e681fbf6ca21d7008159dfaf950f
Elliot Garbus <elliotg2@cox.net>
2019-04-02 17:41:23 -0700

    Updated to display size and position, fixed issue with position

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 """ A test for how Windows are sized to evaluate Windows, Mac and Linux"""
 from kivy.config import Config
 print('Setting Config values prior to start')
-Config.set('graphics', 'width', 800)
+Config.set('graphics', 'width', 400)
 Config.set('graphics', 'height', 400)
 
 # Config.set must be run prior to the app starting, or will have no effect
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty
 from kivy.metrics import dp
 from kivy.core.window import Window
 
@@ -19,21 +19,9 @@ class SizeInput(BoxLayout):
 
 class SizeIt(BoxLayout):
 
-    def config_set(self, w, h):
-        print(f'Using Config.set with width:{w} and height:{h}')
-        Config.set('graphics', 'width', int(w))
-        Config.set('graphics', 'height', int(h))
-
     def wsize(self, w, h):
         print(f'Using Window.size with width:{w} and height:{h}')
         Window.size = (int(w), int(h))
-
-    def config_set_dp(self, w, h):
-        dpw = int(dp(w))
-        dph = int(dp(h))
-        print(f'Using Config.set dp() with width:{dpw} and height:{dph}')
-        Config.set('graphics', 'width', int(dpw))
-        Config.set('graphics', 'height', int(dph))
 
     def wsize_dp(self, w, h):
         dpw = int(dp(w))
@@ -43,8 +31,66 @@ class SizeIt(BoxLayout):
 
 
 class WindowSizeApp(App):
-    pass
+    win_top = NumericProperty()
+    win_left= NumericProperty()
+    win_width = NumericProperty()
+    win_height = NumericProperty()
+
+    def build_config(self, config):
+        config.setdefaults('Window', {'width': 400, 'height': 400})
+        config.setdefaults('Window', {'top': 40, 'left': 40})
+
+    def window_resize(self, win, w, h):
+        print('Window was resized')
+        self.win_width, self.win_height = Window.size
+        self.win_top = Window.top
+        self.win_left = Window.left
+        print(f'resize: Window.size: {Window.size}')
+        print(f'resize: Window.top: {Window.top}, Window.left: {Window.left}')
+
+    def window_close(self, win):
+        print('Window was closed')
+        config = self.config
+        config.set('Window', 'width', Window.size[0])
+        config.set('Window', 'height', Window.size[1])
+        config.set('Window', 'top', self.win_top)
+        config.set('Window', 'left', self.win_left)
+        print(f'close: Window.size: {Window.size}')
+        print(f'close: Window.top: {Window.top}, Window.left: {Window.left}')
+
+    def window_event(self, win):
+        self.win_width, self.win_height = Window.size
+        self.win_top = Window.top
+        self.win_left = Window.left
+
+    def build(self):
+        self.title = 'Window Size and Position Test'
+        Window.minimum_height = 160
+        Window.minimum_width = 300
+        Window.bind(on_request_close=self.window_close)
+        Window.bind(on_draw=self.window_event)
+
+    def on_start(self):
+        # Set Window to previous size and position
+        config = self.config
+        height = config.getdefault('Window', 'height', 160)
+        width = config.getdefault('Window', 'width', 300)
+
+        Window.size = (int(width), int(height))
+        Window.top = int(config.getdefault('Window', 'top', 40))
+        Window.left = int(config.getdefault('Window', 'left', 40))
+
+        print(f'on_start(): Window.size: {Window.size}')
+        print(f'on_start(): Window.top: {Window.top}, Window.left: {Window.left}')
 
 
-if __name__ == '__main__':
-    WindowSizeApp().run()
+    def on_stop(self):
+        # Save Current Window Size and Postion
+        config = self.config
+        # Window position NOT valid in on_stop
+        print(f'on_stop(): Window.size: {Window.size}')
+        print(f'on_stop(): Window.top: {Window.top}, Window.left: {Window.left}')
+        config.write()
+
+
+WindowSizeApp().run()
