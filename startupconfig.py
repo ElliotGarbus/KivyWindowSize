e3b7f281678806f64e5a570501af51115ed2df7a
Elliot Garbus <elliotg2@cox.net>
2019-04-05 05:54:13 -0700

    comment updated

--- startupconfig.py
+++ startupconfig.py
@@ -8,10 +8,10 @@ if found:
     Config.set('graphics', 'width', parser['Window']['width'])
     Config.set('graphics', 'height', parser['Window']['height'])
     Config.set('graphics', 'position', 'custom')
-    Config.set('graphics', 'top', parser['Window']['top']) # find top and left
+    Config.set('graphics', 'top', parser['Window']['top'])  # find top and left
     Config.set('graphics', 'left', parser['Window']['left'])
 else:
-    Config.set('graphics', 'width', 300) # default value match default values in main.py: build_config, on_start
+    Config.set('graphics', 'width', 300)  # default value match default values in main.py: build_config, on_start
     Config.set('graphics', 'height', 200)
     Config.set('graphics', 'position', 'custom')
     Config.set('graphics', 'top', 40)
